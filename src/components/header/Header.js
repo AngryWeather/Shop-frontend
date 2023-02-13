fa98fc32ba39c69be506e41872f038d4fa9d0cb5
Weronika <WerRic96@protonmail.com>
2023-02-13 23:13:31 +0100

    Style search bar

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,10 +1,12 @@
 import "./header.css";
 import Logo from "../logo/Logo";
+import Search from "../search/Search";
 
 const Header = () => {
     return (
       <div className="header">
         <Logo></Logo>
+        <Search></Search>
       </div>  
     );
 }
---
+++ src/components/search/Search.js
@@ -0,0 +1,14 @@
+import "./search.css";
+
+const Search = () => {
+    return(
+        <div id="search-container">
+            <form>
+                <label htmlFor="search"></label>
+                <input type="search"></input>
+            </form>
+        </div>
+    );
+}
+
+export default Search;
