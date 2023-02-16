7b6f36327b8dd1259dcacd158306c31e0bc8ed26
Weronika <WerRic96@protonmail.com>
2023-02-16 23:36:36 +0100

    Create Jewelery component

---
+++ src/components/product-categories/jewelery/jewelery.js
@@ -0,0 +1,27 @@
+import { useEffect, useState } from "react";
+import Product from "../../product/Product";
+
+const Jewelery = () => {
+    const [jewelery, setJewelery] = useState([]);
+
+    useEffect(() => {
+        const loadJewelery = async () => {
+            const response = await fetch("https://fakestoreapi.com/products/category/jewelery");
+            const json = await response.json();
+            setJewelery(json);
+        }
+        
+        loadJewelery().catch(console.error);
+    }, []);
+
+    return (
+        <div className="products">
+            {jewelery.map((product) => (
+                <Product title={product.title} price={product.price} 
+                    description = {product.description} image={product.image} key={product.id}></Product>
+            ))}
+        </div>
+    );
+};
+
+export default Jewelery;
