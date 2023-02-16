71e04c492ad1e30343f5b9aa9b614f51cadc2372
Weronika <WerRic96@protonmail.com>
2023-02-16 23:56:19 +0100

    Display products in /men's clothing

--- src/RouteSwitch.js
+++ src/RouteSwitch.js
@@ -1,6 +1,8 @@
 import { BrowserRouter, Routes, Route  } from "react-router-dom";
 import App from "./App";
 import Electronics from "./components/product-categories/electronics/Electronics";
+import Jewelery from "./components/product-categories/jewelery/Jewelery";
+import MensClothing from "./components/product-categories/mens-clothing/MensClothing";
 
 const RouteSwitch = () => {
     return (
@@ -8,6 +10,8 @@ const RouteSwitch = () => {
             <App></App>
             <Routes>
                 <Route path="/electronics" element={<Electronics/>}></Route>
+                <Route path="/jewelery" element={<Jewelery/>}></Route>
+                <Route path="/men's clothing" element={<MensClothing/>}></Route>
             </Routes>
         </BrowserRouter>
     )
---
+++ src/components/product-categories/mens-clothing/MensClothing.js
@@ -0,0 +1,27 @@
+import { useEffect, useState } from "react";
+import Product from "../../product/Product";
+
+const MensClothing = () => {
+    const [mensClothing, setMensClothing] = useState([]);
+
+    useEffect(() => {
+        const loadMensClothing = async () => {
+            const response = await fetch("https://fakestoreapi.com/products/category/men\'s clothing");
+            const json = await response.json();
+            setMensClothing(json);
+        }
+        
+        loadMensClothing().catch(console.error);
+    }, []);
+
+    return (
+        <div className="products">
+            {mensClothing.map((product) => (
+                <Product title={product.title} price={product.price} 
+                    description = {product.description} image={product.image} key={product.id}></Product>
+            ))}
+        </div>
+    );
+};
+
+export default MensClothing;
