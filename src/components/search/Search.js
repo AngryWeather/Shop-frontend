06ca944e1e933b1dc1d3613c3117051052e30cd3
Weronika <WerRic96@protonmail.com>
2023-02-17 23:25:17 +0100

    Filter search results

--- src/components/search/Search.js
+++ src/components/search/Search.js
@@ -1,9 +1,31 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import Product from "../product/Product";
 import SearchButton from "./search-button/SearchButton";
 import "./search.css";
 
 function Search() {
     const [value, setValue] = useState("");
+    const [products, setProducts] = useState([]);
+    const [match, setMatch] = useState([]);
+    const [isSubmitted, setIsSubmitted] = useState(false);
+
+    useEffect(() => {
+        const loadAllProducts = async () => {
+            const response = await fetch("https://fakestoreapi.com/products");
+            const json = await response.json();
+            setProducts(json);
+        }
+
+        loadAllProducts().catch(console.error);
+    }, []);
+
+    useEffect(() => {
+       setIsSubmitted(true);
+    }, [match])
+
+    const search =  () => {
+        setMatch(products.filter(n => n.title.toLowerCase().includes(value.toLowerCase())));
+    }
 
     const handleChange = event => {
         setValue(event.target.value);
@@ -11,6 +33,7 @@ function Search() {
 
     const handleSubmit = event => {
         event.preventDefault();
+        search();
     }
 
     return(
@@ -20,6 +43,7 @@ function Search() {
                 <input type="search" value={value} onChange={handleChange}></input>
                 <SearchButton></SearchButton>
             </form>
+            {/* {isSubmitted && <Product></Product>} */}
         </div>
     );
 }
