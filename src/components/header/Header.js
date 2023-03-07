331528c1e42f1fe5316f91284f62ee3490cfda0f
Weronika <WerRic96@protonmail.com>
2023-03-07 19:55:54 +0100

    Place Cart in the header

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,12 +1,14 @@
 import "./header.css";
 import Logo from "../logo/Logo";
 import Search from "../search/Search";
+import Cart from "../cart/Cart";
 
 const Header = () => {
     return (
       <div className="header">
         <Logo></Logo>
         <Search></Search>
+        <Cart></Cart>
       </div>  
     );
 }
