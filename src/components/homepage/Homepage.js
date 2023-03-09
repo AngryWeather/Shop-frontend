e731822bdbd657ff2667c8928db746f1643a3e3f
Weronika <WerRic96@protonmail.com>
2023-03-09 16:34:49 +0100

    Display all products on homepage

--- src/components/homepage/Homepage.js
+++ src/components/homepage/Homepage.js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import Product from "../product/Product";
 
 const Homepage = () => {
     const [products, setProducts] = useState([]);
@@ -13,7 +14,15 @@ const Homepage = () => {
         loadAllProducts().catch(console.error);
     }, []);
 
-    
+    return (
+        <div className="products">
+            {products.map((product) => (
+                <Product title={product.title} price={product.price} 
+                    description = {product.description} image={product.image} key={product.id}
+                ></Product>
+            ))}
+        </div>
+    );
 }
 
 export default Homepage;
