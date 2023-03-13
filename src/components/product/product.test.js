a475aa1f072396c59693bdfbb6565837bdacf3d3
Weronika <WerRic96@protonmail.com>
2023-03-14 00:08:26 +0100

    Add test for product

--- src/components/product/product.test.js
+++ src/components/product/product.test.js
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import Product from "./Product";
 
 describe("Product component", () => {
@@ -6,5 +6,12 @@ describe("Product component", () => {
         render(<Product title="Test Product" price="13.5"/>);
         const items = await screen.findAllByRole("listitem");
         expect(items).toHaveLength(4);
+    }),
+
+    it("updates quantity state on input", () => {
+        render(<Product></Product>);
+        const input = screen.getByLabelText("Quantity");
+        fireEvent.change(input, {target: {value: "2"}});
+        expect(input.value).toBe("2");
     })
 })
