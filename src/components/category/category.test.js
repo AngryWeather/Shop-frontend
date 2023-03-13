361328cc8219af971631cdb0d7161152b5924368
Weronika <WerRic96@protonmail.com>
2023-03-13 22:52:18 +0100

    Fix unit tests

--- src/components/category/category.test.js
+++ src/components/category/category.test.js
@@ -1,19 +1,32 @@
 import Category from "./Category"
 import { render } from "@testing-library/react";
+import { MemoryRouter } from "react-router";
 
 describe("Category component", () => {
     it("renders category name: electronics", () => {
-        const {getByRole} = render(<Category category="electronics"/>);
+        const {getByRole} = render(
+        <MemoryRouter>
+            <Category category="electronics"/>
+        </MemoryRouter>
+        );
         expect(getByRole("listitem").textContent).toBe("electronics");
     }),
 
     it("renders category name: jewelery", () => {
-        const {getByRole} = render(<Category category="jewelery"/>);
+        const {getByRole} = render(
+        <MemoryRouter>
+            <Category category="jewelery"/>
+        </MemoryRouter>
+            );
         expect(getByRole("listitem").textContent).toBe("jewelery");
     }),
 
     it("anchor has correct href attribute", () => {
-        const {getByRole} = render(<Category category="electronics"/>);
+        const {getByRole} = render(
+        <MemoryRouter>
+            <Category category="electronics"/>
+        </MemoryRouter>
+        );
         expect(getByRole("link")).toHaveAttribute("href", "/electronics");
     })
 })
--- src/components/logo/Logo.test.js
+++ src/components/logo/Logo.test.js
@@ -1,9 +1,14 @@
 import { render } from "@testing-library/react";
+import { MemoryRouter } from "react-router";
 import Logo from "./Logo";
 
 describe("Logo component", () => {
     it("renders correct heading", () => {
-        const {getByRole} = render(<Logo />);
+        const {getByRole} = render(
+        <MemoryRouter>
+            <Logo />
+        </MemoryRouter>
+        );
         expect(getByRole("heading").textContent).toMatch("Store");
     });
 });
