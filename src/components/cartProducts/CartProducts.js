fcd8a7b3a227eea927b382d4ecdb0193eb9c438f
Weronika <WerRic96@protonmail.com>
2023-03-16 00:21:49 +0100

    Display price in cart

--- src/components/cartProducts/CartProducts.js
+++ src/components/cartProducts/CartProducts.js
@@ -17,12 +17,16 @@ const CartProducts = () => {
     console.log(total);
 
     return (
-        <div className="products">
-            {products.map((elem) => (
-                <CartProduct title={elem.product.title} description={elem.product.description}
-                    image={elem.product.image} price={elem.product.price}
-                    quantity={elem.quantity} key={elem.product.id}></CartProduct>
-            ))}
+        <div>
+            {/* Display total with 2 decimal places */}
+            <p className="total">Total: {total.toFixed(2)}$</p>
+            <div className="products">
+                {products.map((elem) => (
+                    <CartProduct title={elem.product.title} description={elem.product.description}
+                        image={elem.product.image} price={elem.product.price}
+                        quantity={elem.quantity} key={elem.product.id}></CartProduct>
+                ))}
+            </div>
         </div>
     );
 }
