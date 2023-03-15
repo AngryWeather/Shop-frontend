644bb8ae5d8c0802b570d1ac991d55a272428a33
Weronika <WerRic96@protonmail.com>
2023-03-16 00:31:24 +0100

    Round price for prouct to 2 decimal places

--- src/components/cartProduct/CartProduct.js
+++ src/components/cartProduct/CartProduct.js
@@ -7,7 +7,7 @@ const CartProduct = props => {
             <div className="data">
                 <li className="title">{props.title}</li>
                 <li className="price">Quantity: {props.quantity}</li>
-                <li className="price">To pay: {props.price * props.quantity}$</li>
+                <li className="price">To pay: {(props.price * props.quantity).toFixed(2)}$</li>
                 <li className="description">{props.description}</li>
             </div>
         </ol>
