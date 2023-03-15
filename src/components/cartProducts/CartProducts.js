d5e1c97b1af6cb4881bbc775edafefe2686daed5
Weronika <WerRic96@protonmail.com>
2023-03-16 00:04:53 +0100

    Calculate total price for cart products

--- src/components/cartProducts/CartProducts.js
+++ src/components/cartProducts/CartProducts.js
@@ -1,9 +1,20 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { ProductContext } from "../../ProductContext";
 import CartProduct from "../cartProduct/CartProduct";
 
 const CartProducts = () => {
     const {products} = useContext(ProductContext);
+    const [total, setTotal] = useState(0);
+
+    const calculateTotal = () => {
+        setTotal(products.reduce((a, b) => a + b.product.price * b.quantity, 0));
+    }
+
+    useEffect(() => {
+        calculateTotal();
+    }, [])
+
+    console.log(total);
 
     return (
         <div className="products">
