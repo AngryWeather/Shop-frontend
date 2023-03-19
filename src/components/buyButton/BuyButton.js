964396c881a168740ac1fefff3e6a4f771d0de17
Weronika <WerRic96@protonmail.com>
2023-03-19 14:53:19 +0100

    Update quantity of existing product in cart

--- src/components/buyButton/BuyButton.js
+++ src/components/buyButton/BuyButton.js
@@ -5,16 +5,26 @@ import "./buyButton.css";
 const BuyButton = props => {
     // store the product the user wants to buy
     const {products, setProducts} = useContext(ProductContext);
-    console.log(products);
 
-    const productExistsInCart = () => {
-        return products.find(product => product.product.title === props.product.title);
+    const getIndexOfCartProduct = () => {
+        return products.findIndex(product => product.product.title === props.product.title);
     }
 
     const onClickHandler = () => {
-        setProducts(products.concat({product: props.product, quantity: Number(props.quantity)}));
+        let index = getIndexOfCartProduct();
+
+        if (index != -1) {
+            /* instead of adding the same product to the cart
+            copy products array and update quantity object with the new value */
+            let arr = [...products];
+            arr[index]["quantity"] = Number(products[index]["quantity"]) + Number(props.quantity);
+        } else {
+            setProducts(products.concat({product: props.product, quantity: Number(props.quantity)}));
+        }
+
         // reset quantity input field to 1
         props.changeQuantity(1);
+
     }
 
 
