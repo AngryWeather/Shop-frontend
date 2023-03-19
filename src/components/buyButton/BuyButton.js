3677731857dbea03f5bdaf099b94e49d58cd3632
Weronika <WerRic96@protonmail.com>
2023-03-19 11:19:12 +0100

    Change font for anchor tags

--- src/components/buyButton/BuyButton.js
+++ src/components/buyButton/BuyButton.js
@@ -1,10 +1,15 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { ProductContext } from "../../ProductContext";
 import "./buyButton.css";
 
 const BuyButton = props => {
     // store the product the user wants to buy
     const {products, setProducts} = useContext(ProductContext);
+    console.log(products);
+
+    const productExistsInCart = () => {
+        return products.find(product => product.product.title === props.product.title);
+    }
 
     const onClickHandler = () => {
         setProducts(products.concat({product: props.product, quantity: Number(props.quantity)}));
