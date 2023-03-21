628a36007e3b9fafcdecfcdbd594718a1f50d4ff
Weronika <WerRic96@protonmail.com>
2023-03-22 00:31:25 +0100

    Center quantity and 'X' button on smaller screens

--- src/components/quantity/Quantity.js
+++ src/components/quantity/Quantity.js
@@ -1,15 +1,14 @@
-import { useState } from "react";
 import "./quantity.css";
+import "../../center.css";
 
 const Quantity = props => {
-    // const [quantityClass, setQuantityClass] = useState("quantity");
 
     function changeQuantity(e) {
         props.changeQuantity(e.target.value);
     }
 
     return (
-        <div className={props.quantityClass}>
+        <div className={`${props.quantityClass} center`}>
             <label htmlFor="quantity-bought">Quantity</label>
             <input onChange={changeQuantity} id="quantity-bought" type="number" value={props.quantity} min="1"></input>
         </div>
